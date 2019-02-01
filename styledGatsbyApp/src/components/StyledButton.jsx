443fdb4740d35cc7376c69e53bf1705e3fc45f50
Herve Desrosiers <hdesrosiers@stingray.com>
2019-02-01 08:42:30 -0500

    validate styled-components in gatsby project

---
+++ styledGatsbyApp/src/components/StyledButton.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
+
+const StyledButton = props => {
+  return <Button />;
+};
+
+export default StyledButton;
--- styledGatsbyApp/src/pages/index.js
+++ styledGatsbyApp/src/pages/index.js
@@ -1,9 +1,10 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
+import StyledButton from "../components/StyledButton";
 
 const IndexPage = () => (
   <Layout>
@@ -14,8 +15,9 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <StyledButton />
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
