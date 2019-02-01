4245fdd65015ab712b8cb93653289229371a9871
Herve Desrosiers <hdesrosiers@stingray.com>
2019-02-01 12:20:12 -0500

    styled button

--- styledGatsbyApp/src/components/StyledButton.jsx
+++ styledGatsbyApp/src/components/StyledButton.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import styled from "styled-components";
 
 const Button = styled.button`
+  font-family: sans-serif;
   background: transparent;
   border-radius: 3px;
   border: 2px solid palevioletred;
   color: palevioletred;
-  margin: 0 1em;
+  margin: 1em 1em;
   padding: 0.25em 1em;
 `;
 
 const StyledButton = props => {
-  return <Button />;
+  return <Button>Styled Component</Button>;
 };
 
 export default StyledButton;
--- styledGatsbyApp/src/pages/index.js
+++ styledGatsbyApp/src/pages/index.js
@@ -9,14 +9,8 @@ import StyledButton from "../components/StyledButton";
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+
     <StyledButton />
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 );
 
