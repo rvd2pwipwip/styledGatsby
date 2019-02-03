3c5c539aa81b086480923c95522cb5f675ff6f8c
rvd2pwipwip <herve_desrosiers@hotmail.com>
2019-02-03 11:15:25 -0500

    gatsby default starter with styled-components header

---
+++ styledGatsbyApp02/src/components/header.js
@@ -0,0 +1,44 @@
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`;
+
+const Inner = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`;
+
+const Heading = styled.h1`
+  margin: 0px;
+`;
+
+const HeadingLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`;
+
+const Header = ({ siteTitle }) => (
+  <Container>
+    <Inner>
+      <Heading>
+        <HeadingLink to="/">{siteTitle}</HeadingLink>
+      </Heading>
+    </Inner>
+  </Container>
+);
+
+Header.propTypes = {
+  siteTitle: PropTypes.string
+};
+
+Header.defaultProps = {
+  siteTitle: ``
+};
+
+export default Header;
