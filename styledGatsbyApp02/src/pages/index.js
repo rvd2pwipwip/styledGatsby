e898478c59e0a3e57d5851a4236913acf2cfb5fb
Herve Desrosiers <hdesrosiers@stingray.com>
2019-02-04 08:36:28 -0500

    styled-components hero background-image

--- styledGatsbyApp02/src/pages/index.js
+++ styledGatsbyApp02/src/pages/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Link } from "gatsby";
 import styled from "styled-components";
 import Layout from "../components/layout";
+import heroImage from "../images/wallpaper3.jpg";
 // import Image from "../components/image";
 import SEO from "../components/seo";
 
 const Hero = styled.div`
   height: 920px;
-  background: url("../images/wallpaper3.jpg");
+  background: url(${heroImage});
   background-size: cover;
   background-position: center;
   position: relative;
@@ -17,6 +18,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
 
+    {/* <div className="Hero"> */}
     <Hero>
       <div className="HeroGroup">
         <h1>Learn to design and code React apps</h1>
@@ -30,6 +32,7 @@ const IndexPage = () => (
         <Link to="/page-2/">Watch the video</Link>
       </div>
     </Hero>
+    {/* </div> */}
   </Layout>
 );
 
