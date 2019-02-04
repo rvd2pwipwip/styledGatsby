1bd87aef977f9e394008ed154b7b301264020930
Herve Desrosiers <hdesrosiers@stingray.com>
2019-02-04 07:26:01 -0500

    basic styling with css 13:48

--- styledGatsbyApp02/src/pages/index.js
+++ styledGatsbyApp02/src/pages/index.js
@@ -1,21 +1,36 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
+import styled from "styled-components";
+import Layout from "../components/layout";
+// import Image from "../components/image";
+import SEO from "../components/seo";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const Hero = styled.div`
+  height: 920px;
+  background: url("../images/wallpaper3.jpg");
+  background-size: cover;
+  background-position: center;
+  position: relative;
+`;
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
 
-export default IndexPage
+    <Hero>
+      <div className="HeroGroup">
+        <h1>Learn to design and code React apps</h1>
+        <p>
+          Complete courses about the best tools and design systems. Prototype
+          and build apps with React and Swift.
+        </p>
+        {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+        </div> */}
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </Hero>
+  </Layout>
+);
+
+export default IndexPage;
