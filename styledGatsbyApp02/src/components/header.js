320b9b19b7038d48cc54c62dfed0450b6021b0ff
Herve Desrosiers <hdesrosiers@stingray.com>
2019-02-05 08:24:26 -0500

    adaptive layout, css grid 18:00

--- styledGatsbyApp02/src/components/header.js
+++ styledGatsbyApp02/src/components/header.js
@@ -2,35 +2,68 @@ import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
 import styled from "styled-components";
+import devices from "../helpers/devices";
 
-const Container = styled.div`
-  background: rebeccapurple;
-  margin-bottom: 1.45rem;
+const NavBar = styled.div`
+  position: fixed;
+  width: 100%;
+  padding: 50px 0;
+  z-index: 100;
+  @media ${devices.mobileS} {
+    padding: 15px 0;
+  }
 `;
 
-const Inner = styled.div`
+// .NavBar a:nth-child(4) {
+//   display: none;
+// }
+
+const NavBarGroup = styled.div`
+  max-width: 800px;
   margin: 0 auto;
-  max-width: 960px;
-  padding: 1.45rem 1.0875rem;
+  display: grid;
+  grid-template-columns: repeat(5, auto);
+  align-items: center;
+  justify-items: center;
+  @media ${devices.mobileS} {
+    grid-template-columns: repeat(4, auto);
+  }
 `;
 
-const Heading = styled.h1`
-  margin: 0px;
-`;
-
-const HeadingLink = styled(Link)`
+const NavBarLink = styled(Link)`
   color: white;
-  text-decoration: none;
+  font-weight: 700;
+`;
+
+const NavBarButton = styled.button`
+  padding: 8px 20px;
+  font-size: 20px;
+  background: #56ccf2;
+  border: none;
+  font-weight: 700;
+  border-radius: 10px;
+  outline: none;
+  cursor: pointer;
 `;
 
 const Header = ({ siteTitle }) => (
-  <Container>
-    <Inner>
-      <Heading>
-        <HeadingLink to="/">{siteTitle}</HeadingLink>
-      </Heading>
-    </Inner>
-  </Container>
+  <NavBar>
+    <NavBarGroup>
+      <NavBarLink to="/">
+        <img
+          src={require("../images/logo-designcode.svg")}
+          alt="Home"
+          width="30"
+        />
+      </NavBarLink>
+      <NavBarLink to="/courses">Courses</NavBarLink>
+      <NavBarLink to="/downloads">Downloads</NavBarLink>
+      <NavBarLink to="/workshops">Workshops</NavBarLink>
+      <NavBarLink to="/buy">
+        <NavBarButton>Buy</NavBarButton>
+      </NavBarLink>
+    </NavBarGroup>
+  </NavBar>
 );
 
 Header.propTypes = {
--- styledGatsbyApp02/src/pages/index.js
+++ styledGatsbyApp02/src/pages/index.js
@@ -2,37 +2,88 @@ import React from "react";
 import { Link } from "gatsby";
 import styled from "styled-components";
 import Layout from "../components/layout";
-import heroImage from "../images/wallpaper3.jpg";
+// import heroImage from "../images/wallpaper3.jpg";
+import devices from "../helpers/devices";
 // import Image from "../components/image";
 import SEO from "../components/seo";
 
+//https://github.com/facebook/create-react-app/issues/3238
+//require instead of import
 const Hero = styled.div`
   height: 920px;
-  background: url(${heroImage});
+  background: url(${require("../images/wallpaper3.jpg")});
   background-size: cover;
   background-position: center;
   position: relative;
 `;
 
+//https://jsramblings.com/2018/02/04/styled-components-media-queries.html
+const HeroGroup = styled.div`
+  margin: 0 auto;
+  max-width: 500px;
+  text-align: center;
+  padding: 150px 50px;
+  @media ${devices.mobileL} {
+    padding: 100px 20px;
+  }
+`;
+
+const HeroTitle = styled.h1`
+  margin: 0;
+  color: white;
+  font-size: 60px;
+  line-height: 1.2;
+  @media ${devices.mobileL} {
+    font-size: 40px;
+  }
+  @media ${devices.mobileS} {
+    font-size: 36px;
+  }
+`;
+
+const HeroText = styled.p`
+  font-size: 28px;
+  line-height: 1.5;
+  color: rgba(255, 255, 255, 0.8);
+  @media ${devices.mobileL} {
+    font-size: 22px;
+  }
+  @media ${devices.mobileS} {
+    font-size: 18px;
+    margin: 10px 0 40px 0;
+  }
+`;
+
+//https://stackoverflow.com/questions/49639031/styled-components-with-gatsby-link-anchor-tag-css-coloring
+const StyledLink = styled(Link)`
+  font-size: 17px;
+  font-weight: 600;
+  color: white;
+  text-transform: uppercase;
+  background: rgba(0, 0, 0, 0.7);
+  padding: 9px 20px;
+  border: 1px solid rgba(255, 255, 255, 0.25);
+  border-radius: 20px;
+`;
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-
-    {/* <div className="Hero"> */}
     <Hero>
-      <div className="HeroGroup">
-        <h1>Learn to design and code React apps</h1>
-        <p>
+      <HeroGroup>
+        <HeroTitle>
+          Learn to <br /> design and code React apps
+        </HeroTitle>
+        <HeroText>
           Complete courses about the best tools and design systems. Prototype
           and build apps with React and Swift.
-        </p>
+        </HeroText>
         {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
         </div> */}
-        <Link to="/page-2/">Watch the video</Link>
-      </div>
+        <StyledLink to="/page-2/">Watch the video</StyledLink>
+      </HeroGroup>
     </Hero>
-    {/* </div> */}
   </Layout>
 );
 
